5ecadc64b39d425e7cd68c32778902c58bd5ae7f
Ziga Mahne <ziga.mahne@arm.com>
2025-09-25 08:59:03 +0200

    Update memory regions file

--- Examples/Blinky/RTE/Device/STM32U575AIIx/regions_STM32U575I-EV.h
+++ Examples/Blinky/RTE/Device/STM32U575AIIx/regions_STM32U575I-EV.h
@@ -6,12 +6,12 @@
 //------ With VS Code: Open Preview for Configuration Wizard -------------------
 
 // <n> Auto-generated using information from packs
-// <i> Device Family Pack (DFP):   Keil::STM32U5xx_DFP@3.0.0
+// <i> Device Family Pack (DFP):   Keil::STM32U5xx_DFP@3.2.0
 // <i> Board Support Pack (BSP):   Keil::STM32U575I-EV_BSP@1.0.0
 
 // <h> ROM Configuration
 // =======================
-// <h> __ROM0 (is rx memory: Flash from DFP)
+// <h> __ROM0 (is rx memory: Flash-non-secure from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region. Default: 0x08000000
 //   <i> Contains Startup and Vector Table
@@ -24,75 +24,77 @@
 // <h> __ROM1 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __ROM1_BASE 0
+#define __ROM1_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __ROM1_SIZE 0
+#define __ROM1_SIZE 0x0
 // </h>
 
 // <h> __ROM2 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __ROM2_BASE 0
+#define __ROM2_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __ROM2_SIZE 0
+#define __ROM2_SIZE 0x0
 // </h>
 
 // <h> __ROM3 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __ROM3_BASE 0
+#define __ROM3_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __ROM3_SIZE 0
+#define __ROM3_SIZE 0x0
 // </h>
 
 // </h>
 
 // <h> RAM Configuration
 // =======================
-// <h> __RAM0 (is rwx memory: SRAM1_2 from DFP)
+// <h> __RAM0 (is rwx memory: SRAM123-non-secure from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region. Default: 0x20000000
 //   <i> Contains uninitialized RAM, Stack, and Heap
 #define __RAM0_BASE 0x20000000
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
-//   <i> Defines size of memory region. Default: 0x00040000
-#define __RAM0_SIZE 0x00040000
+//   <i> Defines size of memory region. Default: 0x000C0000
+#define __RAM0_SIZE 0x000C0000
 // </h>
 
-// <h> __RAM1 (unused)
+// <h> __RAM1 (is rwx memory: SRAM4-non-secure from DFP)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
-//   <i> Defines base address of memory region.
-#define __RAM1_BASE 0
+//   <i> Defines base address of memory region. Default: 0x28000000
+#define __RAM1_BASE 0x28000000
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
-//   <i> Defines size of memory region.
-#define __RAM1_SIZE 0
+//   <i> Defines size of memory region. Default: 0x00004000
+#define __RAM1_SIZE 0x00004000
 // </h>
 
 // <h> __RAM2 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __RAM2_BASE 0
+#define __RAM2_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __RAM2_SIZE 0
+#define __RAM2_SIZE 0x0
 // </h>
 
 // <h> __RAM3 (unused)
 //   <o> Base address <0x0-0xFFFFFFFF:8>
 //   <i> Defines base address of memory region.
-#define __RAM3_BASE 0
+#define __RAM3_BASE 0x0
 //   <o> Region size [bytes] <0x0-0xFFFFFFFF:8>
 //   <i> Defines size of memory region.
-#define __RAM3_SIZE 0
+#define __RAM3_SIZE 0x0
 // </h>
 
 // </h>
 
 // <n> Resources that are not allocated to linker regions
-// <i> rwx RAM:  SRAM3 from DFP:  BASE: 0x20040000  SIZE: 0x00080000
+// <i> rx ROM:   Flash-secure from DFP:    BASE: 0x0C000000  SIZE: 0x00200000
+// <i> rwx RAM:  SRAM123-secure from DFP:  BASE: 0x30000000  SIZE: 0x000C0000
+// <i> rwx RAM:  SRAM4-secure from DFP:    BASE: 0x38000000  SIZE: 0x00004000
 
 
 #endif /* REGIONS_STM32U575I_EV_H */
